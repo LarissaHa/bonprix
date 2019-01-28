6f3a75b24ae35f0c465a1ba53f6a00bfa90c9e49
Larissa <larissa@gasleuchte.de>
2019-01-28 22:00:38 +0100

    added css template and corrected some errors

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Review
 from django.utils import timezone
 
 # Create your views here.
 def review_list(request):
     reviews = Review.objects.filter(date__lte=timezone.now()).order_by('date')
-    return render(request, 'blog/review_list.html', {'reviews': reviews})
+    return render(request, 'reviews/review_list.html', {'reviews': reviews})
+
+def index(request):
+    return render(request, 'reviews/index.html', {})
 
 def reviews_detail(request, product):
     reviews = Review.objects.filter(date__lte=timezone.now()).filter_by(product_number=product).order_by('date')
