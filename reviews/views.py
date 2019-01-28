f4ab11a3302142f29094de60b5a114305b2e4974
Larissa <larissa@gasleuchte.de>
2019-01-28 21:09:27 +0100

    html and show reviews list

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+def review_list(request):
+    reviews = Review.objects.filter(date__lte=timezone.now()).order_by('date')
+    return render(request, 'blog/review_list.html', {'reviews': reviews})
+
+def reviews_detail(request, product):
+    reviews = Review.objects.filter(date__lte=timezone.now()).filter_by(product_number=product).order_by('date')
